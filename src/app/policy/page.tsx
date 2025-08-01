ef43466796ec4d071db5708db948255737096813
mayowa <mayowamakinde23@gmail.com>
2025-08-01 23:05:14 +0100

    hero UI design

---
+++ src/app/policy/page.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1>Policy page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+        architecto numquam eveniet, nihil ipsum fuga optio perferendis quam,
+        totam dignissimos iure accusantium magni dicta ullam commodi
+        reprehenderit aperiam exercitationem sapiente voluptas praesentium
+        ipsam. Culpa adipisci perferendis suscipit quos perspiciatis delectus ad
+        est ipsam debitis rerum placeat soluta, quo eaque error aliquid aut
+        doloremque, repellat fuga atque amet magni, consequatur unde. Voluptatum
+        deserunt inventore iusto quaerat ipsam quidem sunt delectus sint dolore!
+        Tempora sint facilis sit, aut ad commodi inventore quae impedit delectus
+        ab harum veniam repudiandae beatae nulla asperiores quos consequatur est
+        laboriosam aspernatur rerum autem! Ad culpa eius maxime, ab, laudantium
+        impedit sunt atque rem, iure ut assumenda sit adipisci voluptatum
+        officia excepturi reprehenderit. Nam magni vel, cupiditate voluptates
+        quis dolor! Dolore expedita tempora vitae modi? Perferendis, ratione!
+        Sint impedit saepe, a veniam laudantium quisquam iure inventore optio
+        maiores beatae libero porro perspiciatis obcaecati exercitationem
+        incidunt quam facilis quo culpa. Accusamus dicta cum ut possimus, minima
+        corporis amet error iusto corrupti! Quibusdam reiciendis consequuntur
+        harum minima non culpa molestiae dicta aliquid molestias explicabo
+        quaerat, quis, quia iure id fuga voluptas ipsam natus repellendus! Culpa
+        blanditiis veritatis soluta velit sapiente quae ratione exercitationem,
+        quidem voluptatem quasi, accusantium et consectetur! Molestias porro
+        reprehenderit optio officia impedit qui tempore totam. Eaque, iure
+        aliquid temporibus modi nobis quas sit voluptatem velit quia hic fuga
+        recusandae minus fugit. Optio corrupti nisi incidunt veniam quo ab earum
+        qui! Ea, distinctio modi ipsam qui temporibus id ratione animi
+        repellendus incidunt iusto quam esse itaque voluptatem perferendis
+        architecto eos maiores asperiores, suscipit dolores quidem ipsa
+        cupiditate deserunt in mollitia! Iusto minus, non maxime tenetur aperiam
+        odio quam. Repellendus rerum earum cupiditate quia placeat id atque,
+        similique officiis dolor sunt voluptate commodi reprehenderit ullam vero
+        facilis quo libero soluta sed ducimus fugit. Et quia assumenda quam
+        repellendus dolore.
+      </p>
+    </div>
+  );
+}
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
 export default function page() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Terms and condition</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+        pariatur ab optio minus officiis voluptas exercitationem, facere magnam,
+        vero deserunt consequatur perferendis hic quos. Eveniet et vel, at omnis
+        consequatur officia inventore neque rerum commodi, sit deleniti nostrum
+        corrupti! Quas quae repellat distinctio ducimus voluptatum dolorum
+        magnam, voluptatem mollitia, sint amet architecto rem fugit, quod
+        voluptate perferendis vitae. Alias maiores rerum debitis dolores libero
+        blanditiis officia quisquam! Quia unde ab voluptatibus exercitationem.
+        Vitae harum quidem quos accusamus. Odio, dolorem placeat, labore
+        veritatis, fugit ipsam iure modi tempora cumque error eum nulla
+        voluptate quibusdam expedita! Suscipit obcaecati quaerat sunt cumque
+        eveniet distinctio molestias quidem esse omnis non asperiores doloremque
+        quae inventore unde ex aperiam, numquam placeat quas libero ducimus sit
+        adipisci. Obcaecati ratione explicabo quos aspernatur tenetur facere
+        modi voluptatibus quam animi, ducimus doloremque, iure dolorem
+        distinctio! Maxime pariatur impedit dolor illo! Aliquam possimus minima
+        consequatur cumque quia iure est deleniti voluptatum explicabo similique
+        distinctio quisquam, vel veniam quam porro maiores totam, esse quis
+        autem cum ducimus tenetur! Animi sequi odio incidunt placeat illo
+        officiis esse corrupti, officia ipsam molestiae quas veritatis iure
+        quisquam fugit dolor quod atque ratione! Ipsam perspiciatis tenetur
+        illum itaque! Praesentium laboriosam at iste aliquam eligendi suscipit
+        veritatis deserunt hic amet enim. Eveniet officiis quod natus, dicta
+        beatae nam corrupti deleniti accusamus quam asperiores recusandae sequi
+        eligendi iusto porro aspernatur tempore voluptate ab magnam. Minima
+        porro eveniet vero, ab vel incidunt beatae, nemo dolor inventore error
+        mollitia iure exercitationem assumenda natus molestias tempore fugiat
+        libero optio. Quisquam ea, commodi culpa minima perferendis ipsa magni
+        molestias provident corrupti nesciunt, quibusdam maxime aperiam laborum.
+        Et vel, tempora eligendi laboriosam fugit dolores voluptatem iusto
+        reiciendis exercitationem qui harum quia sed maxime corrupti. Sed quo,
+        cumque, reprehenderit sint ullam ducimus sequi eius quos ipsum enim
+        incidunt. Assumenda beatae eveniet recusandae distinctio!
+      </p>
+    </div>
+  );
 }
